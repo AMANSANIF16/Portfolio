b951d3ebeb5e1e0e33e37ae611648592092ae3f2
AMANSANIF16 <amansanif@gamil.com>
2025-10-02 14:32:58 +0530

    first commit

---
+++ src/config.ts
@@ -0,0 +1,73 @@
+export const siteConfig = {
+  name: "Aman Sanif",
+  title: "A Mechatronics Engineering Student",
+  description: "Portfolio website of Aman Sanif",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "amansanif@gmail.com",
+    linkedin: "https://www.linkedin.com/in/amansanif/",
+    twitter: "https://x.com/amansanif",
+    github: "https://github.com/AMANSANIF16",
+  },
+  aboutMe:
+    "I’m Aman, a passionate Mechatronics Engineering student and aspiring Software Engineer with skills in Java, Python, React, and modern frameworks. I enjoy building efficient, user-focused applications and exploring areas like IoT, machine learning, and AR/VR. With a strong interest in clean code, problem-solving, and continuous learning, I aim to create impactful solutions that blend innovation with practicality",
+  skills: ["Java", "React", "Node.js", "Python", "DSA", "Laravel"],
+  projects: [
+    {
+      name: "Phishing Link Detector",
+      description:
+        "A machine learning – based phishing URL detector that identifies malicious links by analyzing their structure and features. The project helps users stay safe online by classifying links as legitimate or phishing with high accuracy.",
+      link: "https://github.com/AMANSANIF16/phishing_link_detector",
+      skills: ["Machine Learning", "Python", "Model Training"],
+    },
+    {
+      name: "OpenCV based Projects",
+      description:
+        "Master the art of OpenCV with Python through hands-on projects. From image processing to computer vision applications, explore a variety of practical implementations that showcase the power of OpenCV in real-world scenarios.",
+      link: "https://github.com/AMANSANIF16/opencv_projects",
+      skills: ["OpenCV", "Python", "Data Leanring"],
+    },
+    {
+      name: "Starwars Scene Rendering using Unreal Engine 5 ",
+      description:
+        "Created a cinematic scene in Unreal Engine featuring a fully modeled spaceship, dynamic lighting, HDRI backgrounds, 3D animations, and an army of Stormtroopers. Focused on advanced rendering techniques and material customization to achieve a visually immersive experience.",
+      link: "https://www.linkedin.com/posts/amansanif_unrealengine-3danimation-rendering-activity-7273745675241058304-5QEz?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD71_0cBYTFCYj6PItQqQXGpApDyEONhlrQ",
+      skills: ["React", "Node.js", "AWS"],
+    },
+  ],
+  experience: [
+    {
+      company: "B.I. Enterprise",
+      title: "Data Science Intern",
+      dateRange: "Jun 2024 - Jul 2024",
+      bullets: [
+        "Gained hands-on experience in data preprocessing and visualization",
+        "Applied machine learning techniques on real-world datasets",
+        "Contributed to automation and presented insights through dashboards",
+      ],
+    },
+    
+  ],
+  education: [
+    {
+      school: "Sri Krishna College of Engineering and Technology",
+      degree: "B.E. in Mechatronics Engineering",
+      dateRange: "2022 - 2026",
+      achievements: [
+        "Curently maintaining a 7.02 GPA",
+        "Ongoing Semester - 7th",
+        "Present in extra carricular activities",
+      ],
+    },
+    {
+      school: "The Vijay Millennium School",
+      degree: "SCIENCE STREAM (12th)",
+      dateRange: "2022",
+      achievements: [
+        "Completed with 71.6% in CBSE Board Exams",
+        "Represented school in football and developing leadership and discipline.",
+        "Gained early exposure to programming and problem-solving.",
+      ],
+    },
+  ],
+};
